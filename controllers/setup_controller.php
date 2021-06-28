c59a395eb3c81b8c9017e424c16a6da1b101ce0e
Charmaine Khay Lagunsad <charmaine.lagunsad@mynt.xyz>
2021-06-28 12:39:07 +0800

    Added api base in htaccess

--- controllers/setup_controller.php
+++ controllers/setup_controller.php
@@ -1,7 +1,19 @@
 <?php
 
 class SetupController {
+    const DB_CONNECTION =  'host=ec2-176-34-237-141.eu-west-1.compute.amazonaws.com dbname=d1b4oe0cmou81i user=jljbzxjcxicvvn sslmode=require port=5432 password=f608bcd12574fc48e322dcc08ca5dc6829404f3cb598eb36812439392ad2b4d7';
+
     public function test(){
-        echo json_encode('henlow! dont cha worry this is part of learning and brusing up okie? no rush.');
+        echo json_encode('henlow! dont cha worry this is part of learning and brushing up okie? no rush.');
+    }
+
+    public function setup_database(){
+        $db_connection = pg_connect(self::DB_CONNECTION);
+        $stat = pg_connection_status($db_connection);
+        if ($stat === PGSQL_CONNECTION_OK) {
+            echo 'Connection status ok';
+        } else {
+            echo 'Connection status bad';
+        }    
     }
 }
--- views/setup_view.php
+++ views/setup_view.php
@@ -2,6 +2,12 @@
 
 class SetupView {
     public function index() {
-        echo "sup sir";
+        echo '<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <button id="btn-setup">Setup DB</button>
+        <script>
+            jQuery('#btn-setup').click(function(){
+                alert('setup db here');
+            });
+        </script>';
     }
 }
