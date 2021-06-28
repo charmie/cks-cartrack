4c8b558c6ed72da90e439f80b19f2651c3137300
Charmaine Khay Lagunsad <charmaine.lagunsad@mynt.xyz>
2021-06-28 14:40:43 +0800

    Query postresql list of databases

--- controllers/setup_controller.php
+++ controllers/setup_controller.php
@@ -1,19 +1,36 @@
 <?php
 
 class SetupController {
-    const DB_CONNECTION =  'host=ec2-176-34-237-141.eu-west-1.compute.amazonaws.com dbname=d1b4oe0cmou81i user=jljbzxjcxicvvn sslmode=require port=5432 password=f608bcd12574fc48e322dcc08ca5dc6829404f3cb598eb36812439392ad2b4d7';
+    const DB_CONNECTION = null;
+
+    public function __construct(){
+        $this->DB_CONNECTION = pg_connect('host=ec2-176-34-237-141.eu-west-1.compute.amazonaws.com dbname=d1b4oe0cmou81i user=jljbzxjcxicvvn sslmode=require port=5432 password=f608bcd12574fc48e322dcc08ca5dc6829404f3cb598eb36812439392ad2b4d7');
+    }
 
     public function test(){
         echo json_encode('henlow! dont cha worry this is part of learning and brushing up okie? no rush.');
     }
 
     public function setup_database(){
-        $db_connection = pg_connect(self::DB_CONNECTION);
-        $stat = pg_connection_status($db_connection);
+        
+        $stat = pg_connection_status($this->DB_CONNECTION);
         if ($stat === PGSQL_CONNECTION_OK) {
-            echo 'Connection status ok';
+            $this->delete_schema();
         } else {
             echo 'Connection status bad';
         }    
+    }
+
+    private function delete_schema(){
+        $query = 'SELECT datname FROM pg_database';
+        $result = pg_query(self::DB_CONNECTION, $query);
+        if (!$result) {
+            echo "An error occurred.\n";
+            exit;
+        }
+
+        while ($row = pg_fetch_row($result)) {
+            var_dump($row[0]);
+        }
     }
 }
