751ca9c8afe6fae5d9af8464534e8eeae6caf960
Charmie <csorila17@gmail.com>
2021-06-30 16:11:13 +0800

    Debug

--- controllers/car_controller.php
+++ controllers/car_controller.php
@@ -11,6 +11,9 @@ class CarController extends CarModel{
     public function create(){
         echo "<br /><hr />";
         var_dump($_POST);
+        $data = json_decode(file_get_contents('php://input'), true);
+        print_r($data);
+        echo $data["operacion"];
         echo "<br /><hr />";
         if( $_SERVER['REQUEST_METHOD'] == 'POST') {
             $new_values = array(
