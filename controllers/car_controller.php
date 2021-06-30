2ef0a21f84145b09607166a54a5c386098de98dd
Charmie <csorila17@gmail.com>
2021-06-30 16:07:24 +0800

    Debug json post

--- controllers/car_controller.php
+++ controllers/car_controller.php
@@ -9,23 +9,34 @@ class CarController extends CarModel{
     
     // http://ec2-18-207-184-223.compute-1.amazonaws.com/api/car/create  
     public function create(){
-        $new_values = array(
-            'id' => '000001',
-            'model_name' => 'Toyota Camry 2010',
-            'model_type' => 'Camry',
-            'model_brand' => 'Toyota',
-            'model_year' => '2010',
-            'model_date_added' => '',
-            'model_date_modified' => ''
-        );
-
-        $this->car_model->save($new_values);
-
-        $data = array(
-            'status' => 'SUCCESS',
-            'message' => 'This is the create api'
-        );
-        echo json_encode($data);
+        echo "<br /><hr />";
+        var_dump($_POST);
+        echo "<br /><hr />";
+        if( $_SERVER['REQUEST_METHOD'] == 'POST') {
+            $new_values = array(
+                'id' => '000001',
+                'model_name' => 'Toyota Camry 2010',
+                'model_type' => 'Camry',
+                'model_brand' => 'Toyota',
+                'model_year' => '2010',
+                'model_date_added' => '',
+                'model_date_modified' => ''
+            );
+    
+            $this->car_model->save($new_values);
+    
+            $data = array(
+                'status' => 'SUCCESS',
+                'message' => 'This is the create api'
+            );
+            echo json_encode($data);
+        } else {
+            $data = array(
+                'status' => 'FAILED',
+                'message' => 'Method not allowed'
+            );
+        }
+        
     }
 
     // http://ec2-18-207-184-223.compute-1.amazonaws.com/api/car/read
